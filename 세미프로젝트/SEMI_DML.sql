3e14a08e404e7cc9b9c3bc5d88a9bb86d02398e1
BaekUiHeon <135290607+BaekUiHeon@users.noreply.github.com>
2023-08-07 00:03:48 +0900

    게시판수정

--- 세미프로젝트/SEMI_DML.sql
+++ 세미프로젝트/SEMI_DML.sql
@@ -8,3 +8,17 @@ select * from
 (select IDX,SUBJECT,CONTENT,to_char(WDATE,'yyyy-mm-dd'),ID from TBL_BOARD order by IDX) Tb1)
 where rn between ? and ?);
 
+select * from tbl_comment;
+
+insert into tbl_comment (idx,content,id) values();
+
+select * from tbl_like;
+
+select * from tbl_board;
+
+select * from tbl_writer;
+
+select ccidx,depth,step,content,cidx,id,wdate,writer 
+from (select * from tbl_comment where idx=1) join tbl_writer using (id) 
+
+order by step asc,ccidx,step desc;
